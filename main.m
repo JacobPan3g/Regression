384950597027a882379e3e4fc81c674b50f589b4
Jacob <jacob@Jpei.(none)>
2013-05-02 19:57:16 +0800

    v1.0.0 without regularization

--- main.m
+++ main.m
@@ -10,16 +10,16 @@ end
 y = data(2:m, 1);
 X = [ones(m-1, 1), data(2:m, 2:n)];
 theta = zeros(n, 1);
-alpha = 0.01;
+alpha = 0.001;
 
 %computeCost(X, y, theta)
-iter_time = 10000000;
-err = 0.0000001;
+iter_time = 1000000000;
+err = 0.00001;
 [theta, dJ] = gradientDescent(X, y, theta, alpha, iter_time, err);
 %disp(dJ);
 
 res = score(theta, tests);
-csvwrite('result_e-7.csv', res);
+csvwrite('result_e-5-3.csv', res);
 
 
 
